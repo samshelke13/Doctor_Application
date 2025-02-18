10eb879319a8f839cc5a9fd4145d0e1a119a01d7
Samarthm1008616 <samarthm@maveric-systems.com>
2025-02-18 15:26:27 +0530

    Added Images

--- doctor-app/src/app/gallery/gallery.component.ts
+++ doctor-app/src/app/gallery/gallery.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { UiMaterialModule } from '../shared/material.module';
 
 @Component({
   selector: 'app-gallery',
-  imports: [],
+  imports: [UiMaterialModule],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.scss'
 })
